32e252eee87a4a670f965111841c9abf4ad82308
javierrollan <jrollanb@gmail.com>
2021-07-03 13:53:08 +0200

    Do over Structure, Chapter01 finished

---
+++ old/variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char Name[] = "John";
+	int Age = 35;
+	printf("There once was a man named %s\n", Name);
+	printf("He was years %d old\n",Age );
+}
